4caaea0e049a39318c9a8d6a8b2164587b2b04e6
Robert Mill <81169127+RobertVMill@users.noreply.github.com>
2025-01-26 20:31:00 -0500

    new

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,17 +1,17 @@
-import NextAuth from 'next-auth'
+import { AuthOptions } from 'next-auth'
 import EmailProvider from 'next-auth/providers/email'
-import { PrismaAdapter } from "@next-auth/prisma-adapter"
-import { PrismaClient } from "@prisma/client"
+import { PrismaAdapter } from '@next-auth/prisma-adapter'
+import { PrismaClient } from '@prisma/client'
 
 const prisma = new PrismaClient()
 
-export const authOptions = {
+export const authOptions: AuthOptions = {
   adapter: PrismaAdapter(prisma),
   providers: [
     EmailProvider({
       server: {
         host: process.env.EMAIL_SERVER_HOST,
-        port: process.env.EMAIL_SERVER_PORT,
+        port: Number(process.env.EMAIL_SERVER_PORT),
         auth: {
           user: process.env.EMAIL_SERVER_USER,
           pass: process.env.EMAIL_SERVER_PASSWORD
@@ -25,10 +25,9 @@ export const authOptions = {
       // Only allow specific email to sign in
       return user.email === process.env.ALLOWED_EMAIL
     },
-    async session({ session, user }) {
-      if (session?.user) {
-        // Add isAdmin flag to the session
-        session.user.isAdmin = user.email === process.env.ALLOWED_EMAIL
+    async session({ session }) {
+      if (session?.user?.email === process.env.ALLOWED_EMAIL) {
+        session.user.isAdmin = true
       }
       return session
     }
