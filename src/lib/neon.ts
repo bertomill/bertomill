65a899c4f127f84a75087f93d5818c14c4e8e17d
Robert Mill <81169127+RobertVMill@users.noreply.github.com>
2025-01-30 07:50:13 -0500

    removed neon

--- src/lib/neon.ts
+++ src/lib/neon.ts
@@ -1,7 +1,4 @@
-import { Pool } from '@neondatabase/serverless'
-import { PrismaNeon } from '@prisma/adapter-neon'
 import { PrismaClient } from '@prisma/client'
-// import { ws } from '@neon-io/postgres'
 
 declare global {
   let cachedPrisma: PrismaClient
@@ -10,11 +7,9 @@ declare global {
 let prisma: PrismaClient
 
 if (process.env.NODE_ENV === 'production') {
-  const connectionString = process.env.DATABASE_URL!
   prisma = new PrismaClient()
 } else {
   if (!global.cachedPrisma) {
-    const connectionString = process.env.DATABASE_URL!
     global.cachedPrisma = new PrismaClient()
   }
   prisma = global.cachedPrisma
